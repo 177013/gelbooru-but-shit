a52c4aa8bf0fb339e9efd5109275fb4edca3e337
177013 <knichtcore@gmail.com>
2019-08-18 15:06:36 +0200

    chore: use your braces, kids

--- index.php
+++ index.php
@@ -1,41 +1,42 @@
 <?php
-require "inv.header.php";
+require __DIR__ . "/inv.header.php";
+
 if (isset($_GET['page']) && $_GET['page'] != "") {
     if ($_GET['page'] == "account") {
         header("Cache-Control: store, cache");
         header("Pragma: cache");
-        require "includes/header.php";
-        require "includes/account.php";
-    } else if ($_GET['page'] == "reg")
-        require "includes/signup.php";
-    else if ($_GET['page'] == "login")
-        require "includes/login.php";
-    else if ($_GET['page'] == "post")
-        require "includes/posts.php";
-    else if ($_GET['page'] == "history")
-        require "includes/history.php";
-    else if ($_GET['page'] == "account-options")
-        require "includes/account_options.php";
-    else if ($_GET['page'] == "account_profile")
-        require "includes/account_profile.php";
-    else if ($_GET['page'] == "comment")
-        require "includes/comment.php";
-    else if ($_GET['page'] == "search")
-        require "includes/search.php";
-    else if ($_GET['page'] == "favorites")
-        require "includes/favorites.php";
-    else if ($_GET['page'] == "alias")
-        require "includes/alias.php";
-    else if ($_GET['page'] == "reset_password")
-        require "includes/reset_password.php";
-    else if ($_GET['page'] == "forum")
-        require "includes/forum.php";
-    else {
+        require __DIR__ . "/includes/header.php";
+        require __DIR__ . "/includes/account.php";
+    } else if ($_GET['page'] == "reg") {
+        require __DIR__ . "/includes/signup.php";
+    } else if ($_GET['page'] == "login") {
+        require __DIR__ . "/includes/login.php";
+    } else if ($_GET['page'] == "post") {
+        require __DIR__ . "/includes/posts.php";
+    } else if ($_GET['page'] == "history") {
+        require __DIR__ . "/includes/history.php";
+    } else if ($_GET['page'] == "account-options") {
+        require __DIR__ . "/includes/account_options.php";
+    } else if ($_GET['page'] == "account_profile") {
+        require __DIR__ . "/includes/account_profile.php";
+    } else if ($_GET['page'] == "comment") {
+        require __DIR__ . "/includes/comment.php";
+    } else if ($_GET['page'] == "search") {
+        require __DIR__ . "/includes/search.php";
+    } else if ($_GET['page'] == "favorites") {
+        require __DIR__ . "/includes/favorites.php";
+    } else if ($_GET['page'] == "alias") {
+        require __DIR__ . "/includes/alias.php";
+    } else if ($_GET['page'] == "reset_password") {
+        require __DIR__ . "/includes/reset_password.php";
+    } else if ($_GET['page'] == "forum") {
+        require __DIR__ . "/includes/forum.php";
+    } else {
         header("Location:" . $site_url . "/");
         exit;
     }
 } else {
     header("Cache-Control: store, cache");
     header("Pragma: cache");
-    require "includes/index.php";
+    require __DIR__ . "/includes/index.php";
 }
