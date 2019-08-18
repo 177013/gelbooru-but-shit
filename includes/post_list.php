c12769f660bbf148bfe78dbc33af295a88654c53
177013 <knichtcore@gmail.com>
2019-08-18 15:20:00 +0200

    chore: split image and script, improve readability in string building

--- includes/post_list.php
+++ includes/post_list.php
@@ -136,12 +136,35 @@ $domain = $cache->select_domain();
                                         }
                                     }
                                 }
-                                $images .= '<span class="thumb"><a id="p' . $row['id'] . '" href="index.php?page=post&amp;s=view&amp;id=' . $row['id'] . '"><img src="' . $thumbnailManager->makeIfNeeded($row['directory'] . '/' . $row['image']) . '" alt="post" border="0" title="' . $row['tags'] . ' score:' . $row['score'] . ' rating:' . $row['rating'] . '"/></a>
-				<script type="text/javascript">
-				//<![CDATA[
-				posts[' . $row['id'] . '] = {\'tags\':\'' . strtolower(str_replace('\\', "&#92;", str_replace("'", "&#039;", $tags))) . '\'.split(/ /g), \'rating\':\'' . $row['rating'] . '\', \'score\':' . $row['score'] . ', \'user\':\'' . str_replace('\\', "&#92;", str_replace(' ', '%20', str_replace("'", "&#039;", $row['owner']))) . '\'}
-				//]]>
-				</script></span>';
+
+                                $images .= strtr('
+                                    <span class="thumb">
+                                        <a id="p:id" href=":href">
+                                            <img src=":imgSrc" 
+                                                 alt="post" 
+                                                 border="0" 
+                                                 title=":title"/>
+                                        </a>', [
+                                    ':id' => $row['id'],
+                                    ':href' => 'index.php?' . http_build_query([
+                                            'page' => 'post',
+                                            's' => 'view',
+                                            'id' => $row['id'],
+                                        ]),
+                                    ':imgSrc' => $thumbnailManager->makeIfNeeded($row['directory'] . '/' . $row['image']),
+                                    ':title' => $row['tags'] . ' score:' . $row['score'] . ' rating:' . $row['rating'],
+                                ]);
+
+                                $images .= '
+                                    <script type="text/javascript">
+                                    //<![CDATA[
+                                    posts[' . $row['id'] . '] = {\'tags\':\'' . strtolower(str_replace('\\', "&#92;", str_replace("'", "&#039;", $tags))) . '\'.split(/ /g), \'rating\':\'' . $row['rating'] . '\', \'score\':' . $row['score'] . ', \'user\':\'' . str_replace('\\', "&#92;", str_replace(' ', '%20', str_replace("'", "&#039;", $row['owner']))) . '\'}
+                                    //]]>
+                    				</script>
+                                ';
+
+                                $images .= '</span>';
+
                                 ++$tcount;
                             }
                             $result->free_result();
--- includes/posts.php
+++ includes/posts.php
@@ -1,12 +1,12 @@
 <?php
-if ($_GET['s'] == "add")
-    require "includes/post_add.php";
-else if ($_GET['s'] == "view")
-    require "includes/post_view.php";
-else if ($_GET['s'] == "list")
-    require "includes/post_list.php";
-else if ($_GET['s'] == "vote")
-    require "includes/post_vote.php";
-else if ($_GET['s'] == "random")
-    require "post_random.php";
-?>
+if (!empty($_GET['s']) && $_GET['s'] == "add") {
+    require __DIR__ . "/../includes/post_add.php";
+} else if ($_GET['s'] == "view") {
+    require __DIR__ . "/../includes/post_view.php";
+} else if ($_GET['s'] == "list") {
+    require __DIR__ . "/../includes/post_list.php";
+} else if ($_GET['s'] == "vote") {
+    require __DIR__ . "/../includes/post_vote.php";
+} else if ($_GET['s'] == "random") {
+    require __DIR__ . "/post_random.php";
+}
