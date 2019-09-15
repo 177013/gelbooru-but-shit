3d86960bc1b2e4ad8a315b8142938eac391911d2
177013 <knichtcore@gmail.com>
2019-09-15 16:41:15 +0200

    chore: extend autoload to allow Psr4

--- autoload.php
+++ autoload.php
@@ -1,5 +1,14 @@
 <?php
-function __autoload($class)
-{
-    require "classes/$class.class.php";
-}
+
+spl_autoload_register(function(string $class): void {
+    $paths = [
+        __DIR__ . "/classes/$class.class.php",
+        __DIR__ . "/classes/$class.php",
+    ];
+
+    foreach ($paths as $path) {
+        if (file_exists($path)) {
+            require $path;
+        }
+    }
+});
